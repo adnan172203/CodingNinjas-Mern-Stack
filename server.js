c72ce3425981101606adccc2e0b7091c89c5978a
Adnan <adnan172203@gmail.com>
2020-01-08 18:16:22 +0600

    added route

--- server.js
+++ server.js
@@ -8,4 +8,17 @@ app.get('/',(req,res) =>{
     res.send('api running');
 });
 
+//Route 
+const usersRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const profileRoute = require('./routes/profile');
+const postsRoute = require('./routes/posts');
+
+//Define routes
+
+app.use('/api/users', usersRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/posts', postsRoute);
+
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
