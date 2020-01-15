0067b1ac640a21c9fe3b2f831b17824d8c41f3b7
Adnan <adnan172203@gmail.com>
2020-01-15 10:48:13 +0600

    added add education & delete education route and github repos fetch route

--- routes/profile.js
+++ routes/profile.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const auth = require('../middleware/auth');
@@ -132,7 +133,7 @@ router.get('/:userId', async (req, res) => {
   }
 });
 
-//delte profile,user & post
+//delete profile,user & post
 router.delete('/', auth, async (req, res) => {
   try {
     //remove profile
@@ -153,7 +154,6 @@ router.put(
   '/experience',
   [
     auth,
-
     check('title', 'title is required').notEmpty(),
     check('company', 'company is required').notEmpty(),
     check('from', 'From date is required').notEmpty()
@@ -161,16 +161,24 @@ router.put(
   async (req, res) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-      return res.status(400).send({errors:errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
     }
 
-    const { title,company,location,from,to,current, description } = req.body;
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
 
     //value is assigned like this title:title .. but u can write it only title
-    const newExp = { title,company,location,from,to,current, description }
+    const newExp = { title, company, location, from, to, current, description };
 
-    try{
+    try {
       const profile = await Profile.findOne({ user: req.user.id });
 
       profile.experience.unshift(newExp);
@@ -178,11 +186,130 @@ router.put(
       await profile.save();
 
       res.json(profile);
-    }catch(err){
+    } catch (err) {
       console.log(err.message);
       res.status(500).send('Server Error from experience');
     }
   }
 );
+
+//Delete experience from profile
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //get remove index
+    const removeIndex = await profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error from delete experience');
+  }
+});
+
+//Add EDUCATION route
+
+router.put(
+  '/education',
+  [
+    auth,
+    check('school', 'School is required').notEmpty(),
+    check('degree', 'Degree is required').notEmpty(),
+    check('fieldofstudy', 'Field of study is required').notEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    //value is assigned like this title:title .. but u can write it only title
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.education.unshift(newEdu);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error from education');
+    }
+  }
+);
+
+//Delete EDUCATION from profile
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //get remove index
+    const removeIndex = await profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error from delete education');
+  }
+});
+
+
+//get github repo route
+router.get('/github/:username',(req, res)=>{
+  try{
+    const options = {
+      uri:`https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=bbe5069b49bdd66db832&client_secret=08108d667f3016821e56997179a8b0768b6130e7`,
+      method: 'GET',
+      headers: { 'user-agent':'node.js' }
+    }
+
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+
+      if( response.statusCode !== 200 ){
+        return res.status(400).json({msg:'no github profile found'});
+      }
+
+      res.json(JSON.parse(body));
+      
+    });
+  }catch(err){
+    console.log(err.message);
+    res.status(500).send('server error from gihub');
+  }
+});
 
 module.exports = router;
