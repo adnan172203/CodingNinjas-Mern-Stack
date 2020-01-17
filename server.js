c09cb333479de804d76bba5a4db5e295d5530945
Adnan <adnan172203@gmail.com>
2020-01-17 18:49:38 +0600

    install create-react-app, added multiple packages and did some routing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 //db
 const {connectDB} = require('./config/db');
